de1596278d71a3cbab7d1f8f2f6a8be39d6e8e8d
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-06 23:56:30 +0200

    Imagenes

--- subida.sh
+++ subida.sh
@@ -4,7 +4,13 @@ git add README.md &&
 git commit -m "Introducción a ANSIBLE" &&
 git push origin master &&
 git add . &&
+git add images/. &&
+git commit -m "Imagenes" &&
+git push origin master &&
 git commit -m "Recursos ansible" &&
-git push origin master
+git push origin master &&
+git add subida.sh &&
+git commit -m "Script de subida de archivos" &&
+git sube
 sleep 2
 echo "Trabajo terminado"
