c3df0d5636d9b77499f91a8a5ec67fd760350b25
chunche95 <37621958+chunche95@users.noreply.github.com>
2022-07-09 18:49:57 +0200

    Contenido 'tradicional'

---
+++ test/index.php
@@ -0,0 +1,28 @@
+<?php require('./custom/header.php') ?>
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Me -->
+							<article id="home" class="panel intro">
+								<header>
+									<h1>Human Computing</h1>
+									<p>DevOps - Automatización de operaciones en  nube pública.</p>
+								</header>
+								<a href="#work" class="jumplink pic">
+									<span class="arrow icon fa-chevron-right"><span>See my work</span></span>
+									<img src="images/Intro.jpg" alt="" />
+								</a>
+							</article>
+
+						<!-- Work -->
+							<article id="work" class="panel">
+								<header>
+									<h2>Work</h2>
+								</header>
+								<p>
+									<h3>Initial requirements.</h3>
+									This site contains all the helping for ejects the DevOps in Public Clouds. <br>									
+									<a href="contenido.php" rel="noopener noreferrer">Show me awesome content</a>
+								</p>								
+							</article>
+<?php require('./custom/footer.php') ?>
